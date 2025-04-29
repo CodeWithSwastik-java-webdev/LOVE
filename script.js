992d60a03e45bed56ab66e61f7f4b99a569ef400
CodeWithSwastik-java-webdev <swastik2580@gmail.com>
2025-04-29 06:37:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let button = document.getElementById("btn");
 
 button.addEventListener("click", () => {
-    document.querySelector(".box").innerHTML = `<b>Ans - 100% obviously💗</b>`
+    document.querySelector(".box").innerHTML = `<b>Ans - 100% obviously💗</b>`;
+    document.getElementById("btn").setAttribute("style" , "display: none");
 })
